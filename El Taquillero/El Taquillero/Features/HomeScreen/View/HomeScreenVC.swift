3ececd050c4e2d73c84bf465cd807543b38bf5a3
Andrés Fonseca <dfonsecar@unal.edu.co>
2024-11-06 06:07:13 -0300

    UPDATE: Partial commit. Setup collectionViewContainer and added String Catalog for LocalizedStrings.

--- El Taquillero/El Taquillero/Features/HomeScreen/View/HomeScreenVC.swift
+++ El Taquillero/El Taquillero/Features/HomeScreen/View/HomeScreenVC.swift
@@ -12,9 +12,13 @@ class HomeScreenVC: UIViewController, StoryboardInfo {
     static var storyboard = "HomeScreen"
     static var identifier = "HomeScreenVC"
 
+    @IBOutlet weak var label: UILabel!
+    
+    let testText = String(localized: "TestPhrase")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.label.text = testText
     }
     
     func setupVIPER() {
